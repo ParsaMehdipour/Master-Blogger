2bac789983be762b767e369178736bd3159ee5c6
Parsa <mehdipourparsa@gmail.com>
2021-04-16 02:47:55 +0430

    feat: add article category domain

---
+++ MB.Domain/ArticleCategoryAgg/ArticleCategory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MB.Domain.ArticleCategoryAgg
+{
+    public class ArticleCategory
+    {
+        public long Id { get; private set; }
+        public string Title { get; private set; }
+        public bool isDeleted { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        public ArticleCategory(string title)
+        {
+            Title = title;
+            isDeleted = false;
+            CreationDate=DateTime.Now;
+        }
+
+        public void Edit(string title)
+        {
+            Title = title;
+        }
+
+        public void Deleted()
+        {
+            isDeleted = true;
+        }
+    }
+}
