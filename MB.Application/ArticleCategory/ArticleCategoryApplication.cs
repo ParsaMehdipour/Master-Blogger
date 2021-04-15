d701390b1a4866b2d636e75f9ca19a25772b13af
Parsa <mehdipourparsa@gmail.com>
2021-04-16 02:58:41 +0430

    feat: add article application class

---
+++ MB.Application/ArticleCategory/ArticleCategoryApplication.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using MB.Application.Contracts.ArticleCategory;
+
+namespace MB.Application.ArticleCategory
+{
+    public class ArticleCategoryApplication:IArticleCategoryApplication
+    {
+        public List<ArticleCategoryViewModel> List()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
