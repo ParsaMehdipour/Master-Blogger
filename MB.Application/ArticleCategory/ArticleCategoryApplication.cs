a344676ef0a1262e8fa6a8e19e6d68897603bceb
Parsa <mehdipourparsa@gmail.com>
2021-04-18 01:18:32 +0430

    refactor: impelemented i article category application

--- MB.Application/ArticleCategory/ArticleCategoryApplication.cs
+++ MB.Application/ArticleCategory/ArticleCategoryApplication.cs
@@ -1,11 +1,40 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using MB.Application.Contracts.ArticleCategory;
+using MB.Domain.ArticleCategoryAgg;
 
 namespace MB.Application.ArticleCategory
 {
     public class ArticleCategoryApplication:IArticleCategoryApplication
     {
+        private readonly IArticleCategoryRepository _repository;
+
+        public ArticleCategoryApplication(IArticleCategoryRepository repository)
+        {
+            _repository = repository;
+        }
         public List<ArticleCategoryViewModel> List()
+        {
+           var list = new List<ArticleCategoryViewModel>();
+
+           var categoryArticles = _repository.GetAll();
+
+           foreach (var item in categoryArticles)
+           {
+               list.Add(new ArticleCategoryViewModel
+               {
+                   Id = item.Id,
+                   Title = item.Title,
+                   IsDeleted = item.isDeleted,
+                   CreationDate = item.CreationDate.ToString(CultureInfo.InvariantCulture)
+               });
+           }
+
+           return list;
+
+        }
+
+        public void Create(CreateArticleCategory command)
         {
             throw new System.NotImplementedException();
         }
