ec4294f099b2f38c1e38eeb93e4f50f66fa3bdf5
Parsa <mehdipourparsa@gmail.com>
2021-04-18 00:59:05 +0430

    feat: add i article category repository

---
+++ MB.Domain/ArticleCategoryAgg/IArticleCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MB.Domain.ArticleCategoryAgg
+{
+    public interface IArticleCategoryRepository
+    {
+        void Create();
+
+        
+    }
+}
