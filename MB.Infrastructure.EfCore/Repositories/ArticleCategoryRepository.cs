904484a8cb64cea44657af1f5686a8fb752c59b4
Parsa <mehdipourparsa@gmail.com>
2021-04-18 01:21:58 +0430

    feat: add article category repository

---
+++ MB.Infrastructure.EfCore/Repositories/ArticleCategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MB.Domain.ArticleCategoryAgg;
+
+namespace MB.Infrastructure.EfCore.Repositories
+{
+    public class ArticleCategoryRepository:IArticleCategoryRepository
+    {
+        private readonly MasterBloggerDbContext _context;
+
+        public ArticleCategoryRepository(MasterBloggerDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(ArticleCategory category)
+        {
+            _context.ArticleCategories.Add(category);
+            _context.SaveChanges();
+        }
+
+        public List<ArticleCategory> GetAll()
+        {
+            return _context.ArticleCategories.ToList();
+        }
+    }
+}
