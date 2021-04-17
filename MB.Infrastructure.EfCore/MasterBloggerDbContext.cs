2863e9157bf6757db61e3753f538d806ecbdcbc6
Parsa <mehdipourparsa@gmail.com>
2021-04-18 00:58:40 +0430

    feat: add infrastructure layer

---
+++ MB.Infrastructure.EfCore/MasterBloggerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MB.Domain.ArticleCategoryAgg;
+using Microsoft.EntityFrameworkCore;
+
+namespace MB.Infrastructure.EfCore
+{
+    public class MasterBloggerDbContext : DbContext
+    {
+        public MasterBloggerDbContext(DbContextOptions<MasterBloggerDbContext> options)
+             : base(options)
+        {
+
+        }
+
+        public DbSet<ArticleCategory> ArticleCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
