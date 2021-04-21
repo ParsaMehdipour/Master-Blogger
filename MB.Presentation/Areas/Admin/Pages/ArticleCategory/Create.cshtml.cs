e8fbbd82cfc83e2c4c4407d6dc6da7eebb254a24
Parsa <mehdipourparsa@gmail.com>
2021-04-21 20:48:04 +0430

    refactor: remove unused namespaces

--- MB.Presentation/Areas/Admin/Pages/ArticleCategory/Create.cshtml.cs
+++ MB.Presentation/Areas/Admin/Pages/ArticleCategory/Create.cshtml.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using MB.Application.Contracts.ArticleCategory;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
