7797cfe4281fc12e9003272109e9130f18bc9756
Parsa <mehdipourparsa@gmail.com>
2021-04-21 20:06:22 +0430

    feat: add create article category page

---
+++ MB.Presentation/Areas/Admin/Pages/ArticleCategory/Create.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MB.Application.Contracts.ArticleCategory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MB.Presentation.Areas.Admin.Pages.ArticleCategory
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IArticleCategoryApplication _application;
+
+        public CreateModel(IArticleCategoryApplication application)
+        {
+            _application = application;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public RedirectToPageResult OnPost(CreateArticleCategory command)
+        {
+            _application.Create(command);
+            return RedirectToPage("./List");
+        }
+    }
+}
