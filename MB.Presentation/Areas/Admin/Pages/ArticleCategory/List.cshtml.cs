6bdef09c6269b51cab937d702914b8a2a44c7cf9
Parsa <mehdipourparsa@gmail.com>
2021-04-24 02:02:59 +0430

    refactor: add on post activate , on post remove handler

--- MB.Presentation/Areas/Admin/Pages/ArticleCategory/List.cshtml.cs
+++ MB.Presentation/Areas/Admin/Pages/ArticleCategory/List.cshtml.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Net.Sockets;
 using MB.Application.Contracts.ArticleCategory;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MB.Presentation.Areas.Admin.Pages.ArticleCategory
@@ -11,13 +13,26 @@ namespace MB.Presentation.Areas.Admin.Pages.ArticleCategory
         private readonly IArticleCategoryApplication _application;
 
         public ListModel(IArticleCategoryApplication application)
-
         {
             _application = application;
         }
+
         public void OnGet()
         {
             ArticleCategoryViewModels = _application.List();
         }
+
+        public RedirectToPageResult OnPostRemove(long id)
+        {
+            _application.Remove(id);
+            return RedirectToPage("./List");
+        }
+
+        public RedirectToPageResult OnPostActivate(long id)
+        {
+            _application.Activate(id);
+            return RedirectToPage("./List");
+        }
+
     }
 }
