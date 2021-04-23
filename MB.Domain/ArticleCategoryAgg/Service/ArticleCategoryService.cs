3245db5356b36fa07d4220c9c082e4d63fc5b255
Parsa <mehdipourparsa@gmail.com>
2021-04-24 02:31:51 +0430

    feat: i article category service impelementation

---
+++ MB.Domain/ArticleCategoryAgg/Service/ArticleCategoryService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MB.Domain.ArticleCategoryAgg.Service
+{
+    class ArticleCategoryService : IArticleCategoryService
+    {
+        private readonly IArticleCategoryRepository _repository;
+
+        public ArticleCategoryService(IArticleCategoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void CheckDuplication(string title)
+        {
+            if(_repository.Exists(title))
+                throw new Exception();
+        }
+    }
+}
