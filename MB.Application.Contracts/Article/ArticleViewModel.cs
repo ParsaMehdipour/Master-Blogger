6d0f9453c04a79204998e9dd8444c2d7fdaa0fdf
Parsa <mehdipourparsa@gmail.com>
2021-04-27 02:12:53 +0430

    feat : add article view model

---
+++ MB.Application.Contracts/Article/ArticleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MB.Application.Contracts.Article
+{
+    public class ArticleViewModel
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string ArticleCategory { get; set; }
+        public string ShortDescription { get; set; }
+        public string CreationDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
