7a4acfbbea731bed9941f55aefe282c07edc7d4c
Parsa <mehdipourparsa@gmail.com>
2021-04-27 02:12:16 +0430

    refactor : add get list method to article application

--- MB.Application/Article/ArticleApplication.cs
+++ MB.Application/Article/ArticleApplication.cs
@@ -1,8 +1,20 @@
-﻿using MB.Application.Contracts.Article;
+﻿using System.Collections.Generic;
+using MB.Application.Contracts.Article;
+using MB.Domain.ArticleAgg;
 
 namespace MB.Application.Article
 {
     public class ArticleApplication:IArticleApplication
     {
+        private readonly IArticleRepository _repository;
+
+        public ArticleApplication(IArticleRepository repository)
+        {
+            _repository = repository;
+        }
+        public List<ArticleViewModel> GetList()
+        {
+            return _repository.GetList();
+        }
     }
 }
