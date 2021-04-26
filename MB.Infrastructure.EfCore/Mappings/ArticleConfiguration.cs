0d7e9d7390da31ebae056f653367a0fad62ad88d
Parsa <mehdipourparsa@gmail.com>
2021-04-26 13:27:30 +0430

    feat : add article mapping configuration

---
+++ MB.Infrastructure.EfCore/Mappings/ArticleConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using MB.Domain.ArticleAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MB.Infrastructure.EfCore.Mappings
+{
+    public class ArticleConfiguration:IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.ToTable("Articles");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title);
+
+            builder.Property(x => x.ShortDescription);
+
+            builder.Property(x => x.Content);
+
+            builder.Property(x => x.Image);
+
+            builder.Property(x => x.IsDeleted);
+
+            builder.Property(x => x.CreationDate);
+
+            builder.Property(x => x.Title);
+
+            builder.HasOne(x => x.ArticleCategory)
+                .WithMany(x => x.Articles)
+                .HasForeignKey(x => x.ArticleCategoryId);
+        }
+    }
+}
