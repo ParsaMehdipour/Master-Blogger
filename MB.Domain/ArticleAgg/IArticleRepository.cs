076c0f54fc63c30e5ccc00a74c02b442f16a5b09
Parsa <mehdipourparsa@gmail.com>
2021-04-27 02:11:52 +0430

    refactor : add get list method to i article repository interface

--- MB.Domain/ArticleAgg/IArticleRepository.cs
+++ MB.Domain/ArticleAgg/IArticleRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace MB.Domain.ArticleAgg
-{
-    public class IArticleRepository
-    {
+﻿using System.Collections.Generic;
+using MB.Application.Contracts.Article;
 
+namespace MB.Domain.ArticleAgg
+{
+    public interface IArticleRepository
+    {
+        List<ArticleViewModel> GetList();
     }
 }
