aa82453eddd391a5c4a78daa021e01b1168e53b2
Parsa <mehdipourparsa@gmail.com>
2021-04-26 13:27:06 +0430

    feat: add article repository class

---
+++ MB.Infrastructure.EfCore/Repositories/ArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using MB.Domain.ArticleAgg;
+
+namespace MB.Infrastructure.EfCore.Repositories
+{
+    public class ArticleRepository:IArticleRepository
+    {
+        private readonly MasterBloggerDbContext _context;
+
+        public ArticleRepository(MasterBloggerDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
