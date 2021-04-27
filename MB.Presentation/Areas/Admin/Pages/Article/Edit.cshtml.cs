265b182e61c502df0c0ee33d728aff5503b3d035
Parsa <mehdipourparsa@gmail.com>
2021-04-27 21:17:25 +0430

    feat : add article edit page

---
+++ MB.Presentation/Areas/Admin/Pages/Article/Edit.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MB.Application.Contracts.Article;
+using MB.Application.Contracts.ArticleCategory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MB.Presentation.Areas.Admin.Pages.Article
+{
+    public class EditModel : PageModel
+    {
+        private readonly IArticleApplication _articleApplication;
+        private readonly IArticleCategoryApplication _articleCategoryApplication;
+        public List<SelectListItem> ArticleCategories { get; set; }
+        [BindProperty] public EditArticle Article { get; set; }
+
+        public EditModel(IArticleApplication articleApplication,IArticleCategoryApplication articleCategoryApplication)
+        {
+            _articleApplication = articleApplication;
+            _articleCategoryApplication = articleCategoryApplication;
+        }
+
+        public void OnGet(long id)
+        {
+            ArticleCategories = _articleCategoryApplication.List()
+                .Select(x => new SelectListItem(x.Title, x.Id.ToString()))
+                .ToList();
+
+            Article = _articleApplication.Get(id);
+        }
+
+        public RedirectToPageResult OnPost()
+        {
+            _articleApplication.Edit(Article);
+            return RedirectToPage("./List");
+        }
+    }
+}
