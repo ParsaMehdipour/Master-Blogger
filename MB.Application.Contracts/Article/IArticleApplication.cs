e451e573b69c74ef48f2b65c61f3cac9fbe0d18c
Parsa <mehdipourparsa@gmail.com>
2021-04-27 21:19:07 +0430

    refactor : add save and get method to i article application

--- MB.Application.Contracts/Article/IArticleApplication.cs
+++ MB.Application.Contracts/Article/IArticleApplication.cs
@@ -5,6 +5,8 @@ namespace MB.Application.Contracts.Article
     public interface IArticleApplication
     {
         List<ArticleViewModel> GetList();
+        EditArticle Get(long id);
         void Create(CreateArticle command);
+        void Edit(EditArticle command);
     }
 }
