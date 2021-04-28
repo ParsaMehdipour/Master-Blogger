66123951e767bc84c3844e0ac2f405d9128dfd84
Parsa <mehdipourparsa@gmail.com>
2021-04-28 22:36:32 +0430

    feat : impelemented i article validator service

---
+++ MB.Domain/ArticleAgg/Services/ArticleValidatorService.cs
@@ -0,0 +1,19 @@
+﻿using MB.Domain.ArticleCategoryAgg.Exceptions;
+
+namespace MB.Domain.ArticleAgg.Services
+{
+    public class ArticleValidatorService : IArticleValidatorService
+    {
+        private readonly IArticleRepository _repository;
+
+        public ArticleValidatorService(IArticleRepository repository)
+        {
+            _repository = repository;
+        }
+        public void CheckDuplication(string title)
+        {
+            if(_repository.Exists(title))
+                throw new DuplicationException();
+        }
+    }
+}
