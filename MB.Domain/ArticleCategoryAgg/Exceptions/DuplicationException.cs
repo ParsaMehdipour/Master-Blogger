92ee66d791cf73fbed99462d81071b8cb02f41ea
Parsa <mehdipourparsa@gmail.com>
2021-04-28 22:38:37 +0430

    feat : add duplication exception for article

--- MB.Domain/ArticleCategoryAgg/Exceptions/DuplicationException.cs
+++ MB.Domain/ArticleCategoryAgg/Exceptions/DuplicationException.cs
@@ -4,6 +4,12 @@ namespace MB.Domain.ArticleCategoryAgg.Exceptions
 {
     public class DuplicationException:Exception
     {
+
+        public DuplicationException()
+        :base("This Record Already Exists")
+        {
+
+        }
         public DuplicationException(string message)
             :base(message)
         {
