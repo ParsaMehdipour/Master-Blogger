743a6be2e41e469bd71abb7d1b1a8e8cafb34d26
Parsa <mehdipourparsa@gmail.com>
2021-04-29 21:39:24 +0430

    feat : add comment domain

---
+++ MB.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MB.Domain.ArticleAgg;
+
+namespace MB.Domain.CommentAgg
+{
+    public class Comment
+    {
+        public long Id { get; private set; }
+        public string Name { get; private set; }
+        public string Message { get; private set; }
+        public int Status { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        public long ArticleId { get; private set; }
+        public Article Article { get; set; }
+
+        public Comment()
+        {
+            
+        }
+
+        public Comment(long id, string name, string message, long articleId)
+        {
+            Id = id;
+            Name = name;
+            Message = message;
+            ArticleId = articleId;
+            Status = Statuses.New;
+            CreationDate=DateTime.Now;
+        }
+    }
+}
