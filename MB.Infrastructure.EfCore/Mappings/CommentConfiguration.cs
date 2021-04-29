e75dba18ae0ba6793ca45a7621be287870cf7d04
Parsa <mehdipourparsa@gmail.com>
2021-04-29 22:06:39 +0430

    feat : add comment mapping

---
+++ MB.Infrastructure.EfCore/Mappings/CommentConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using MB.Domain.CommentAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MB.Infrastructure.EfCore.Mappings
+{
+    public class CommentConfiguration:IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("Comments");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name);
+            builder.Property(x=>x.Email);
+            builder.Property(x => x.CreationDate);
+            builder.Property(x => x.Message);
+            builder.Property(x => x.Status);
+
+            builder.HasOne(x => x.Article)
+                .WithMany(x => x.Comments)
+                .HasForeignKey(x => x.ArticleId);
+        }
+    }
+}
