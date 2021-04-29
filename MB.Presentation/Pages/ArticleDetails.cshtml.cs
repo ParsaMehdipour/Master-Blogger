cb02ac5e0e5be2c257fbce56b9c7afc110890184
Parsa <mehdipourparsa@gmail.com>
2021-04-29 12:49:13 +0430

    feat : add article details page

--- MB.Presentation/Pages/ArticleDetails.cshtml.cs
+++ MB.Presentation/Pages/ArticleDetails.cshtml.cs
@@ -1,14 +1,22 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
+﻿using MB.Infrastructure.Query;
+using MB.Infrastructure.Query.Service;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MB.Presentation.MVCCore.Pages
+namespace MB.Presentation.Pages
 {
     public class ArticleDetailsModel : PageModel
     {
+        private readonly IArticleQuery _query;
+        public ArticleQueryView Article { get; set; }
+
+        public ArticleDetailsModel(IArticleQuery query)
+        {
+            _query = query;
+        }
+
         public void OnGet(long id)
         {
-
+            Article = _query.GetOneArticle(id);
         }
     }
 }
