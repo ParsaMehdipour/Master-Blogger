f14137c61a70fc013d163fcd629e3a160a5b174c
Parsa <mehdipourparsa@gmail.com>
2021-04-29 21:39:38 +0430

    feat : add statuses class

---
+++ MB.Domain/CommentAgg/Statuses.cs
@@ -0,0 +1,11 @@
+﻿namespace MB.Domain.CommentAgg
+{
+    public static class Statuses
+    {
+        public const int New  = 0;
+
+        public const int Confirmed  = 1;
+
+        public const int Canceled  = 2;
+    }
+}
