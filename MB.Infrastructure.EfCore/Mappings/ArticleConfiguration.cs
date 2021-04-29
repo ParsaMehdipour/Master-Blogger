c1ae37d4cdad43e32541ac758cccbb6110885ec8
Parsa <mehdipourparsa@gmail.com>
2021-04-29 22:06:25 +0430

    refactor : add comment relation to article mapping

--- MB.Infrastructure.EfCore/Mappings/ArticleConfiguration.cs
+++ MB.Infrastructure.EfCore/Mappings/ArticleConfiguration.cs
@@ -28,6 +28,10 @@ namespace MB.Infrastructure.EfCore.Mappings
             builder.HasOne(x => x.ArticleCategory)
                 .WithMany(x => x.Articles)
                 .HasForeignKey(x => x.ArticleCategoryId);
+
+            builder.HasMany(x => x.Comments)
+                .WithOne(x => x.Article)
+                .HasForeignKey(x => x.ArticleId);
         }
     }
 }
