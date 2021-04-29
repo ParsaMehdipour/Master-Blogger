405a87f8bef0be7b548c6407005a5ee8b8a23369
Parsa <mehdipourparsa@gmail.com>
2021-04-29 11:37:28 +0430

    feat : add i article query interface

---
+++ MB.Infrastructure.Query/Service/IArticleQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MB.Infrastructure.Query.Service
+{
+    public interface IArticleQuery
+    {
+        List<ArticleQueryView> GetArticles();
+    }
+}
