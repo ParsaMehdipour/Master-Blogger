3b68d804221b546aa211010be72dc64ed4df911e
Parsa <mehdipourparsa@gmail.com>
2021-04-29 11:37:05 +0430

    feat : add article query

---
+++ MB.Infrastructure.Query/Service/ArticleQuery.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using MB.Infrastructure.EfCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MB.Infrastructure.Query.Service
+{
+    public class ArticleQuery : IArticleQuery
+    {
+        private readonly MasterBloggerDbContext _context;
+
+        public ArticleQuery(MasterBloggerDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ArticleQueryView> GetArticles()
+        {
+            return _context.Articles.Include(x => x.ArticleCategory)
+                .Select(x => new ArticleQueryView
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    ShortDescription = x.ShortDescription,
+                    Content = x.Content,
+                    CreationDate = x.CreationDate.ToString(CultureInfo.InvariantCulture),
+                    Image = x.Image,
+                    ArticleCategory = x.ArticleCategory.Title
+                }).ToList();
+        }
+    }
+}
