83e53f991e2b37db6612fa964564e85376999863
Parsa <mehdipourparsa@gmail.com>
2021-04-29 11:41:41 +0430

    refactor : show impelement article ui

--- MB.Presentation/Pages/Index.cshtml.cs
+++ MB.Presentation/Pages/Index.cshtml.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections.Generic;
+using MB.Infrastructure.Query;
+using MB.Infrastructure.Query.Service;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MB.Presentation.MVCCore.Pages
+namespace MB.Presentation.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IArticleQuery _query;
+        public List<ArticleQueryView> Articles { get; set; }
+
+        public IndexModel(IArticleQuery query)
+        {
+            _query = query;
+        }
+
         public void OnGet()
         {
-
+            Articles = _query.GetArticles();
         }
     }
 }
