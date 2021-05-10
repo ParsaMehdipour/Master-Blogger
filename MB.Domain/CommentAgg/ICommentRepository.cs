fa2b6240a3e47b76667b32a87ee0665f8b852286
Parsa <mehdipourparsa@gmail.com>
2021-05-11 01:36:26 +0430

    refactor : add get list method to i comment repository interface

--- MB.Domain/CommentAgg/ICommentRepository.cs
+++ MB.Domain/CommentAgg/ICommentRepository.cs
@@ -1,7 +1,11 @@
-﻿namespace MB.Domain.CommentAgg
+﻿using System.Collections.Generic;
+using MB.Application.Contracts.Comment;
+
+namespace MB.Domain.CommentAgg
 {
     public interface ICommentRepository
     {
+        List<CommentViewModel> GetList();
         void CreateAndSave(Comment comment);
     }
 }
