b50d4f83c36fa95d2edf18ac1bcfe3f58b8fcf5f
Parsa <mehdipourparsa@gmail.com>
2021-05-10 23:29:31 +0430

    refactor : add method added to comment application

--- MB.Application/Comment/CommentApplication.cs
+++ MB.Application/Comment/CommentApplication.cs
@@ -1,9 +1,21 @@
 ﻿using MB.Application.Contracts.Comment;
+using MB.Domain.CommentAgg;
 
 namespace MB.Application.Comment
 {
     public class CommentApplication:ICommentApplication
     {
+        private readonly ICommentRepository _repository;
 
+        public CommentApplication(ICommentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Add(AddComment command)
+        {
+            var comment = new Domain.CommentAgg.Comment(command.Name, command.Email, command.Message, command.ArticleId);
+            _repository.CreateAndSave(comment);
+        }
     }
 }
