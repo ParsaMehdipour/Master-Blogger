bff2571f6139ebebb57988767bd5052907596ee8
Parsa <mehdipourparsa@gmail.com>
2021-05-10 23:32:12 +0430

    refactor : add on post method to article details page

--- MB.Presentation/Pages/ArticleDetails.cshtml.cs
+++ MB.Presentation/Pages/ArticleDetails.cshtml.cs
@@ -1,5 +1,7 @@
-﻿using MB.Infrastructure.Query;
+﻿using MB.Application.Contracts.Comment;
+using MB.Infrastructure.Query;
 using MB.Infrastructure.Query.Service;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MB.Presentation.Pages
@@ -7,16 +9,25 @@ namespace MB.Presentation.Pages
     public class ArticleDetailsModel : PageModel
     {
         private readonly IArticleQuery _query;
+        private readonly ICommentApplication _commentApplication;
         public ArticleQueryView Article { get; set; }
 
-        public ArticleDetailsModel(IArticleQuery query)
+        public ArticleDetailsModel(IArticleQuery query,ICommentApplication commentApplication)
         {
             _query = query;
+            _commentApplication = commentApplication;
         }
 
         public void OnGet(long id)
         {
             Article = _query.GetOneArticle(id);
+        }
+
+        public RedirectToPageResult OnPost(AddComment command)
+        {
+            _commentApplication.Add(command);
+
+            return RedirectToPage("./Index");
         }
     }
 }
