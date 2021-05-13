eb0c520e3e7909de1fdc5fa59fd116b13fa9014e
Parsa <mehdipourparsa@gmail.com>
2021-05-13 13:01:49 +0430

    feat : add i repository generic interface

---
+++ 01.Framework/Infrastructure/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using _01.Framework.Domain;
+
+namespace _01.Framework.Infrastructure
+{
+    public interface IRepository<in TKey,TEntity> where TEntity : BaseDomain<TKey>
+    {
+        TEntity Get(TKey id); // ==> Article Get(long id)
+        List<TEntity> GetAll(); // ==> List<Article> GetAll()
+        void Create(TEntity entity); // ==> Void Create(Article article)
+        bool Exists(Expression<Func<TEntity, bool>> expression); // ==> bool Exists(x => x.Title == title)
+    }
+}
