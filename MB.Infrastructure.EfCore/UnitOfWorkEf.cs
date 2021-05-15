d1a7077ffbf1652e99f89d197c4a6125298953f0
Parsa <mehdipourparsa@gmail.com>
2021-05-15 18:36:46 +0430

    feat : impelementation of i unit of work

---
+++ MB.Infrastructure.EfCore/UnitOfWorkEf.cs
@@ -0,0 +1,30 @@
+﻿using _01.Framework.Infrastructure;
+
+namespace MB.Infrastructure.EfCore
+{
+    public class UnitOfWorkEf : IUnitOfWork
+    {
+        private readonly MasterBloggerDbContext _context;
+
+        public UnitOfWorkEf(MasterBloggerDbContext context)
+        {
+            _context = context;
+        }
+
+        public void BeginTran()
+        {
+            _context.Database.BeginTransaction();
+        }
+
+        public void CommitTran()
+        {
+            _context.SaveChanges();
+            _context.Database.CommitTransaction();
+        }
+
+        public void RollBack()
+        {
+            _context.Database.RollbackTransaction();
+        }
+    }
+}
